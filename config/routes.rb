f2153ff6c901428d3595d664abac4fd2e88d2f80
Brian <briandsalemi@gmail.com>
2019-10-09 16:50:52 -0400

    modified config routes to include create and show routes for users controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :cart_plants
   resources :carts
   resources :plants
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:create, :show]
+  
 end
