a7f95807b2ec093afd490f71d750397d3e15a2bc
Brian <briandsalemi@gmail.com>
2019-10-09 16:46:07 -0400

    added association relationships between models to cart, plant, and user models

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,13 @@
 class Cart < ApplicationRecord
+    belongs_to :user 
+    has_many :cart_plants 
+    has_many :plants, through: :cart_plants
+
+    def total 
+        sum = 0
+        self.plants.each do |plant|
+            sum += plant.price
+        end 
+        sum
+    end
 end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,2 +1,4 @@
 class Plant < ApplicationRecord
+    has_many :cart_plants, dependent: :destroy
+    has_many :carts, through: :cart_plants
 end
