f3bcca0021592918be92801882446cbd1b904d30
Brian <briandsalemi@gmail.com>
2019-10-09 14:08:38 -0400

    seeded database with all plant instances

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,99 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Plant.destroy_all
+
+zz_plant = Plant.create(name: "ZZ Plant", species: "Zamioculcas Zamiifolia", price: 110, exp_level: "Beginner", size: "Large", light_required: "Low to Bright Indirect", image: "https://bloomscape.com/wp-content/uploads/2019/03/bloomscape_product-zz-plant-slate-1-324x393.jpg", pet_friendly: false)
+
+burgundy_rubber = Plant.create(name: "Burgundy Rubber Tree", species: "Ficus Elastica 'Burgundy'", price: 110, exp_level: "Intermediate", size: "Large", light_required: "Low to Bright Indirect", image: "https://bloomscape.com/wp-content/uploads/2019/03/bloomscape_product-burgundy-rubber-tree-charcoal-2-324x393.jpg", pet_friendly: false)
+
+wandering_jew = Plant.create(name: "Wandering Jew", species: "Tradescantia Zebrina", price: 35, exp_level: "Beginner", size: "Small", light_required: "Bright Indirect", image: "https://bloomscape.com/wp-content/uploads/2019/04/bloomscape_product-wandering-jew-slate-324x393.jpg", pet_friendly: true)
+
+fiddle_leaf = Plant.create(name: "Fiddle Leaf Fig", species: "Ficus lyrata", price: 155, exp_level: "Advanced", size: "X-Large", light_required: "Bright Indirect", image: "https://bloomscape.com/wp-content/uploads/2019/05/bloomscape_product-fiddle-leaf-fig-slate-2-e1558203121213-324x394.jpg", pet_friendly: false)
+
+kimberly_queen = Plant.create(name: "Kimberly Queen Fern", species: "Nephrolepis Obliterata", price: 65, exp_level: "Intermediate", size: "Medium", light_required: "Medium to Bright Indirect", image: "https://bloomscape.com/wp-content/uploads/2019/05/bloomscape_product-kimberly-queen-fern-slate-2-324x393.jpg", pet_friendly: true) 
+
+neon_pothos = Plant.create(name: "Neon Pothos", species: "Epipremnum Aureum", price: 20, exp_level: "Beginner", size: "Mini", light_required: "Low to Medium Indirect", image: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_neon-pothos_grant_blush_5_1500x.progressive.jpg?v=1566928489", pet_friendly: false)
+
+snake_plant = Plant.create(name: "Petite Snake Plant", species: "Sansevieria", price: 25, exp_level: "Beginner", size: "Mini", light_required: "Medium Indirect", image: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_potted-plant_cylindrical-snake-plant_ezra_mint_1500x.progressive.jpg?v=1560530525", pet_friendly: false)
+
+silver = Plant.create(name: "Silver Philodendron", species: "Scindapsus Pictus", price: 25, exp_level: "Beginner", size: "Mini", light_required: "Medium Indirect", image: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_potted-plant_silver-philodendron_grant_mustard-4_1500x.progressive.jpg?v=1565297788", pet_friendly: false)
+
+majesty = Plant.create(name: "Majesty Palm", species: "Ravenea Rivularis", price: 150, exp_level: "Intermediate", size: "X-Large", light_required: "Low to Bright Indirect", image: "https://bloomscape.com/wp-content/uploads/2019/03/bloomscape_product-majesty-palm-stone-324x393.jpg", pet_friendly: true) 
+
+bird_paradise = Plant.create(name: "Bird of Paradise", species: "Strelitzia Nicolai", price: 150, exp_level: "Intermediate", size: "X-Large", light_required: "Bright Indirect to Bright Direct", image: "https://bloomscape.com/wp-content/uploads/2019/04/bloomscape_product-bird-of-paradise-stone-324x393.jpg", pet_friendly: false)
+
+monstera = Plant.create(name: "Monstera", species: "Monstera Deliciosa", price: 60, exp_level: "Beginner", size: "Medium", light_required: "Bright Indirect", image: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_potted-plant_monstera-6_hyde_mint_7_1500x.progressive.jpg?v=1564151052", pet_friendly: false)
+
+alocasia_polly = Plant.create(name: "Alocasia Polly", species: "Alocasia 'Polly'", price: 40, exp_level: "Advanced", size: "Small", light_required: "Medium Indirect", image: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_alocasia-polly_august_blush_4_1500x.progressive.jpg?v=1566848403", pet_friendly: false)
+
+dracaena = Plant.create(name: "Dracaena Colorama", species: "Dracaena Marginata 'Colorama'", price: 110, exp_level: "Intermediate", size: "Large", light_required: "Bright Indirect", image: "https://bloomscape.com/wp-content/uploads/2019/03/bloomscape_product-dracaena-colorama-charcoal-1-324x393.jpg", pet_friendly: false)
+
+hedgeho_aloe = Plant.create(name: "Hedgehog Aloe", species: "Aloe Humilis 'Hedgehog'", price: 35, exp_level: "Beginner", size: "Small", light_required: "Bright Indirect to Full Sun", image: "https://bloomscape.com/wp-content/uploads/2019/04/bloomscape_product-hedgehog-aloe-clay-324x393.jpg", pet_friendly: false)
+
+bird_nest = Plant.create(name: "Bird's Nest Fern", species: "Asplenium Nidus", price: 35, exp_level: "Intermediate", size: "Small", light_required: "Low to Bright Indirect", image: "https://bloomscape.com/wp-content/uploads/2019/04/bloomscape-product-birds-nest-fern-stone-324x393.jpg", pet_friendly: true)
+
+ponytail = Plant.create(name: "Ponytail Palm", species: "Beaucarnea Recurvata", price: 65, exp_level: "Beginner", size: "Medium", light_required: "Bright Indirect to Bright Direct", image: "https://bloomscape.com/wp-content/uploads/2019/03/bloomscape_product-ponytail-palm-slate-324x393.jpg", pet_friendly: true) 
+
+peperomia = Plant.create(name: "Peperomia Ginny", species: "Peperomia Clusiifolia 'Ginny'", price: 35, exp_level: "Beginner", size: "Small", light_required: "Low to Bright Indirect", image: "https://bloomscape.com/wp-content/uploads/2019/03/bloomscape-product-peperomia-ginny-charcoal-324x393.jpg", pet_friendly: true)
+
+aloe = Plant.create(name: "Aloe Vera", species: "Aloe Vera", price: 35, exp_level: "Beginner", size: "Mini", light_required: "Bright Indirect to Full Sun", image: "http://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_aloe-vera_prospect_blush_4_1500x.progressive.jpg?v=1567675325", pet_friendly: false)
+
+pilea = Plant.create(name: "Pilea Peperomioides", species: "Pilea Peperomioides", price: 40, exp_level: "Beginner", size: "Mini", light_required: "Bright Indirect to Bright Direct", image: "http://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_Grant_PaleRed_Pilea-4_8d08356e-43dd-44dc-9044-997e6751b136_1500x.progressive.jpg?v=1564157115", pet_friendly: false)
+
+maranta = Plant.create(name: "Maranta", species: "Maranta Leuconeura", price: 45, exp_level: "Intermediate", size: "Small", light_required: "Medium to Bright Indirect", image: "http://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_maranta_hyde_blush_5_1500x.progressive.jpg?v=1566853958", pet_friendly: false)
+
+aglaonema = Plant.create(name: "Aglaonema Indo Princess", species: "Aglaonema Hybrida 'Indo Princess'", price: 110, exp_level: "Beginner", size: "Large", light_required: "Low to Bright Indirect", image: "https://bloomscape.com/wp-content/uploads/2019/08/bloomscape_product_indo-princess-aglaonema-indigo-324x395.jpg", pet_friendly: false)
+
+xanadu = Plant.create(name: "Philodendron Xanadu", species: "Philodendron X 'Xanadu'", price: 110, exp_level: "Beginner", size: "Large", light_required: "Medium to Bright Indirect", image: "https://bloomscape.com/wp-content/uploads/2019/04/bloomscape_product-xanadu-stone-1-324x393.jpg", pet_friendly: false)
+
+marginata = Plant.create(name: "Dracaena Marginata Open Weave", species: "Dracaena Reflexa Var Angustifolia", price: 165, exp_level: "Beginner", size: "X-Large", light_required: "Low to Bright Indirect", image: "https://bloomscape.com/wp-content/uploads/2019/03/bloomscape_product-dracaena-open-weave-clay-324x393.jpg", pet_friendly: false)
+
+emerald = Plant.create(name: "Emerald Ripple Cactus", species: "Euphorbia Eritrea Emerald Ripples", price: 40, exp_level: "Beginner", size: "Medium", light_required: "Bright Indirect to Bright Direct", image: "https://bloomscape.com/wp-content/uploads/2019/04/bloomscape_product-emerald-ripple-cactus-indigo-324x395.jpg", pet_friendly: false)
+
+calathea = Plant.create(name: "Calathea Medallion", species: "Calathea Roseapicta", price: 45, exp_level: "Intermediate", size: "Medium", light_required: "Low to Bright Indirect", image: "https://bloomscape.com/wp-content/uploads/2019/03/bloomscape_product-calathea-medallion-charcoal-324x393.jpg", pet_friendly: true) 
+
+janet_craig = Plant.create(name: "Dracaena Janet Craig", species: "Dracaena Deremensis", price: 165, exp_level: "Beginner", size: "X-Large", light_required: "Low to Bright Indirect", image: "https://bloomscape.com/wp-content/uploads/2019/04/bloomscape_product-janet-craig-indigo-324x393.jpg", pet_friendly: false)
+
+neon_prayer = Plant.create(name: "Neon Prayer Plant", species: "Maranta Leuconeura 'Marisela'", price: 45, exp_level: "Beginner", size: "Medium", light_required: "Low to Bright Indirect", image: "https://bloomscape.com/wp-content/uploads/2019/09/bloomscape_product_neon-prayer-plant-4-324x395.jpg", pet_friendly: false)
+
+parlor_palm = Plant.create(name: "Parlor Palm", species: "Chamaedorea Elegans", price: 55, exp_level: "Beginner", size: "Medium", light_required: "Low to Medium Indirect", image: "https://bloomscape.com/wp-content/uploads/2019/03/bloomscape_product-ponytail-palm-slate-2-1140x1382-1-324x393.jpg", pet_friendly: true)
+
+spider_plant = Plant.create(name: "Spider Plant", species: "Chlorophytum Comosum", price: 35, exp_level: "Beginner", size: "Small", light_required: "Low to Bright Indirect", image: "https://bloomscape.com/wp-content/uploads/2019/05/bloomscape-product-spider-plant-stone-2-324x393.jpg", pet_friendly: true)
+
+red_prayer = Plant.create(name: "Red Prayer Plant", species: "Maranta Leuconeura", price: 35, exp_level: "Intermediate", size: "Medium", light_required: "Low to Bright Indirect", image: "https://bloomscape.com/wp-content/uploads/2019/04/bloomscape-product-red-prayer-plant-stone-324x393.jpg", pet_friendly: true)
+
+orbifolia = Plant.create(name: "Orbifolia Plant", species: "Calathea Orbifolia", price: 50, exp_level: "Intermediate", size: "Medium", light_required: "Low to Bright Indirect", image:"https://bloomscape.com/wp-content/uploads/2019/03/bloomscape_product-orbifolia-indigo-324x393.jpg", pet_friendly: true) 
+
+crocodile_fern = Plant.create(name: "Crocodile Fern", species: "Microsorum Musifolium", price: 55, exp_level: "Intermediate", size: "Medium", light_required: "Low to Bright Indirect", image:"https://bloomscape.com/wp-content/uploads/2019/10/bloomscape_product_crocodile-fern_indigo-324x395.jpg", pet_friendly: true)
+
+prickly_pear = Plant.create(name: "Prickly Pear Cactus", species: "Opuntia", price: 55, exp_level: "Beginner", size: "Medium", light_required: "Bright Indirect to Bright Direct", image: "https://bloomscape.com/wp-content/uploads/2019/04/bloomscape_product-prickly-pear-cactus-stone-324x393.jpg", pet_friendly: true)
+
+money_tree = Plant.create(name: "Money Tree", species: "Pachira Aquatica", price: 40, exp_level: "Beginner", size: "Small", light_required: "Medium to Bright Indirect", image: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_money-tree_dolores_mint_5_1500x.progressive.jpg?v=1567782951", pet_friendly: true)
+
+calathea_rattlesnake = Plant.create(name: "Calathea Rattlesnake", species: "Calathea Lancifolia", price: 55, exp_level: "Beginner", size: "Medium", light_required: "Medium Indirect", image: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_calathea-rattlesnake_variant_medium_hyde_cream_1500x.progressive.jpg?v=1569490523", pet_friendly: true)
+
+peperomia_marble = Plant.create(name: "Peperomia Marble", species: "Peperomia Obtusifolia", price: 30, exp_level: "Beginner", size: "Small", light_required: "Medium Indirect", image: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_peperomia-obtusifolia-plant-marble_high-line_pale-red_5_1500x.progressive.jpg?v=1551205440", pet_friendly: true)
+
+hoya_heart = Plant.create(name: "Hoya Heart", species: "Hoya Kerrii", price: 20, exp_level: "Beginner", size: "Mini", light_required: "Bright Indirect to Bright Direct", image: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_hoya-heart_hyde_mint_mini_1500x.progressive.jpg?v=1569006834", pet_friendly: true)
+
+peperomia_green = Plant.create(name: "Peperomia Green", species: "Peperomia Obtusifolia", price: 30, exp_level: "Beginner", size: "Small", light_required: "Medium to Bright Indirect", image: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_peperomia-obtusfolia-green_variant_small_hyde_black_1500x.progressive.jpg?v=1569424577", pet_friendly: true)
+
+staghorn_fern = Plant.create(name: "Staghorn Fern", species: "Platycerium Superbum", price: 40, exp_level: "Beginner", size: "Small", light_required: "Medium to Bright Indirect", image: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_houseplant_staghorn-fern_august_blush_4_1500x.progressive.jpg?v=1537548908", pet_friendly: true)
+
+pinstripe_calathea = Plant.create(name: "Pinstripe Calathea", species: "Calathea Ornata", price: 35, exp_level: "Intermediate", size: "Small", light_required: "Medium Indirect", image: "https://cdn.shopify.com/s/files/1/0150/6262/products/gallery_the-sill_calathea-pinstripe_grant_black_5_1500x.progressive.jpg?v=1555966436", pet_friendly: true)
+
+calathea_freddie = Plant.create(name: "Calathea Freddie", species: "Calathea concinna Freddie", price: 55, exp_level: "Beginner", size: "Medium", light_required: "Medium Indirect", image: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_potted-plant_calathea-freddie-6_prospect_blush-7_1500x.progressive.jpg?v=1566420915", pet_friendly: true)
+
+echeveria_lola = Plant.create(name: "Echeveria Lola", species: "Echeveria 'Lola'", price: 25, exp_level: "Beginner", size: "Mini", light_required:"Bright Indirect to Full Sun", image: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_echeveria-plant_ezra_mint_2.5_1500x.progressive.jpg?v=1567072654", pet_friendly: true)
+
+haworthia_zebra = Plant.create(name: "Haworthia Zebra", species: "Haworthia Fasciata", price: 25, exp_level: "Beginner", size: "Mini", light_required: "Bright Indirect to Full Sun", image: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_haworthia-succulent-zebra-plant_dolores_dark-green_4_1500x.progressive.jpg?v=1567782658", pet_friendly: true)
+
+echeveria = Plant.create(name: "Echeveria", species: "Echeveria", price: 25, exp_level: "Beginner", size: "Mini", light_required: "Bright Indirect to Full Sun", image: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_echeveria-succulent-plant_hyde_mint_4_1500x.progressive.jpg?v=1566420085", pet_friendly: true) 
+
+philodendron_prince = Plant.create(name: "Philodendron Prince of Orange", species: "Philodendron Hybrida 'Prince of Orange'", price: 65, exp_level: "Beginner", size: "Medium", light_required: "Low to Bright Indirect", image: "https://bloomscape.com/wp-content/uploads/2019/09/bloomscape_product_philodendron-prince-of-orange_-324x395.jpg", pet_friendly: false)
+
+yucca_cane = Plant.create(name: "Yucca Cane", species: "Yucca Filamentosa", price: 125, exp_level: "Beginner", size: "Large", light_required: "Medium to Bright Indirect", image: "https://bloomscape.com/wp-content/uploads/2019/08/bloomscape-product-yucca-cane-indigo-324x395.jpg", pet_friendly: false)
+
+croton_petra = Plant.create(name: "Croton Petra", species: "Codiaeum Variegatum 'Petra'", price: 110, exp_level: "Advanced", size: "Large", light_required: "Bright Indirect to Bright Direct", image: "https://bloomscape.com/wp-content/uploads/2019/09/bloomscape_product_croton-petra-324x395.jpg", pet_friendly: false)
+
+ficus_danielle = Plant.create(name: "Ficus Danielle", species: "Ficus Benjamina 'Danielle'", price: 155, exp_level: "Advanced", size: "X-Large", light_required: "Medium to Bright Indirect", image: "https://bloomscape.com/wp-content/uploads/2019/03/bloomscape_product-ficus-danielle-clay-324x393.jpg", pet_friendly: false)
+
+panda_plant = Plant.create(name: "Panda Plant", species: "Kalanchoe Tomentosa", price: 20, exp_level: "Beginner", size: "Mini", light_required: "Bright Indirect to Full Sun", image: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_panda-plant_ezra_mint_2.5_1500x.progressive.jpg?v=1567630601", pet_friendly: false)
