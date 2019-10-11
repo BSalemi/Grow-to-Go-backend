0f70d319382927935ae4df328490503317cf43a2
Brian <briandsalemi@gmail.com>
2019-10-11 13:51:05 -0400

    added the necessary routes to config routes for cart_plants and cart controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :cart_plants
-  resources :carts
+  resources :cart_plants, only [:index, :create, :destroy, :show]
+  resources :carts,  only: [:index, :show]
   resources :plants, only: [:index, :show]
   resources :users, only: [:create, :show]
+
+  post '/checkout' => 'carts#checkout'
   
 end
