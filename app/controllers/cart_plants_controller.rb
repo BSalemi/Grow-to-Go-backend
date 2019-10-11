0668a56f6641efa387a549f60180454e5a03ce90
Brian <briandsalemi@gmail.com>
2019-10-11 13:48:53 -0400

    added index, show, and destroy methods to the cart_plants route

--- app/controllers/cart_plants_controller.rb
+++ app/controllers/cart_plants_controller.rb
@@ -1,2 +1,43 @@
 class CartPlantsController < ApplicationController
+
+    def index
+        cart_plants = CartPlant.all 
+        
+        render json: cart_plants, except: [:created_at, :updated_at]
+    end 
+
+    def create 
+        cart_plant = CartPlant.create(cart_plant_params)
+        cart = Cart.find(cart_plant_params[:cart_id])
+        user = cart.user
+        
+        render json: user, :include => {
+            carts: {
+                except: [:created_at, :updated_at], 
+                methods: :total, 
+                include: {
+                    cart_plants:{ 
+                        include: :plant
+                }},
+            }
+        }, except: [:created_at, :updated_at]
+    end 
+   
+    def destroy 
+        cart_plant = CartPlant.find(params[:id])
+        user = cart_plant.cart.user
+        cart_plant.destroy
+        
+        render json: user, :include => {
+            carts: {
+                except: [:created_at, :updated_at], 
+                methods: :total, 
+                include: {
+                    cart_plants:{ 
+                        include: :plant
+                }},
+            }
+        }, except: [:created_at, :updated_at]
+    end 
+    
 end
