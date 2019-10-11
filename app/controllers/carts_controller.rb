7d4445a30a4dd065d3d8a1d527c00298532a1672
Brian <briandsalemi@gmail.com>
2019-10-11 13:49:41 -0400

    added index, show, and checkout methods to cart controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,44 @@
 class CartsController < ApplicationController
+    def index 
+        carts = Cart.all 
+        
+        render json: carts, :except => [:created_at, :updated_at]
+        render json: carts(:include => {
+            :user => {:only => [:name]},
+        }), :except => [:created_at, :updated_at]
+    end 
+
+    def show 
+        cart = Cart.find_by(id: params[:id])
+        if cart 
+           
+            render json: cart(:include => {
+                user: {:only => [:name]},
+            }), except: [:created_at, :updated_at]
+        else
+            render json: {message: "Cart not found."}
+        end 
+    end 
+
+    def checkout
+        cart = Cart.find(params[:id])
+        cart.checkout = true 
+        cart.save
+        user = cart.user 
+        
+        new_cart = Cart.create(user_id: user.id)
+        
+        render json: user, :include => {
+            carts: {
+                except: [:created_at, :updated_at], 
+                methods: :total, 
+                include: {
+                    cart_plants:{ 
+                        include: :plant
+                    }
+                },
+            },
+        }, except: [:created_at, :updated_at]
+    end 
+    
 end
