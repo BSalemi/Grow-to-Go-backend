56ec9e86f9d7500991cc11d0668779e6e927fb09
Brian <briandsalemi@gmail.com>
2020-02-11 09:51:16 -0500

    created show route for reviews controller, created destroy route for reviews controller to handle deleting a review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,27 @@
 class ReviewsController < ApplicationController
 
     def index
-        reviews = Review.select{|review| review.plant_id == params[:plant_id]}
-        render json: reviews, except: [:created_at, :updated_at]
-    end 
-
-    def show
-        reviews = Review.select{|review| review.plant_id == params[:plant_id]}
+        reviews = Review.all
         render json: reviews, except: [:created_at, :updated_at]
     end 
 
     def create 
          review = Review.create(title: params[:title], body: params[:body], user_id: params[:user_id], plant_id: params[:plant_id] )
          render json: review, except: [:created_at, :updated_at]
+    end 
+
+    def show 
+        review = Review.find(params[:id])
+        if review 
+            render json: review, except: [:created_at, :updated_at]
+        else  
+            render json: {message: "Review not found."}
+        end 
+
+    end 
+
+    def destroy
+        review = Review.find(params[:id]) 
+        review.destroy
     end 
 end
